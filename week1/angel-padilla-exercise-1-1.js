e041a4b42281fa19fd35f76747783bc9e1a2b603
Angel Padilla <aapadilla@my365.bellevue.edu>
2019-11-27 18:09:52 -0600

    Adding week1 exercise 1.1 solution containing two errors.

---
+++ week1/angel-padilla-exercise-1-1.js
@@ -0,0 +1,24 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Angel Padilla
+; Date: 11-27-2019
+; Modified By: <Your name>
+; Description: This program is created to contains two errors
+;===========================================
+*/ 
+
+var person = {
+    name: null,
+    speak: function(){
+        if(name != null){
+            console.log("Hello my name is " + name);    
+        }else{
+            console.log("Please assign a value to name.");
+        }
+        
+    }
+}
+
+person = "Angel";
+person.speak(person.name);
