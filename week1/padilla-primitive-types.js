ef89f568e13cb59ca0afb0a77638406f834b5d4a
Angel Padilla <aapadilla@my365.bellevue.edu>
2019-11-28 23:11:14 -0600

    exercise 1.4 primitive types

---
+++ week1/padilla-primitive-types.js
@@ -0,0 +1,27 @@
+/*
+============================================
+; Title: Assignment 1.4 Primitive Types
+; Author: Angel Padilla
+; Date: 11-28-2019
+; Modified By: <Your name>
+; Description: Apps purpose is to create and log multiple variables
+;===========================================
+*/ 
+
+// variable creation to later log
+var firstName = "Angel";
+var lastName = "Padilla";
+var age = "30";
+var street = "9090 Education Drive";
+var city = "Omaha";
+var zipCode = 54545;
+
+// log each variable's value
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
+
+
