f48fca11410b44681ef521b8886c3efacdd45f51
Angel Padilla <aapadilla@my365.bellevue.edu>
2019-11-30 02:54:02 -0600

    updated the console

--- week1/padilla-types-values-and-variables.js
+++ week1/padilla-types-values-and-variables.js
@@ -32,15 +32,15 @@ var hireDate2 = new Date("2012-2-12").toLocaleDateString();
 var hireDate3 = new Date("2013-3-13").toLocaleDateString();
 
 // logging each employee in one console log using the backslash n "\n" as line break
-console.log(firstName1, lastName1, address1, payRate1, hireDate1 + "\n" +
-            firstName2, lastName2, address2, payRate2, hireDate2 + "\n" +
-            firstName3, lastName3, address3, payRate3, hireDate3 + "\n");
+console.log("Employee 1:", firstName1, lastName1, "lives at", address1, "with an pay rate of", payRate1, "and was hired on", hireDate1 + "\n" +
+            "Employee 2:", firstName2, lastName2, "lives at", address2, "with an pay rate of", payRate2, "and was hired on", hireDate2 + "\n" +
+            "Employee 3:", firstName3, lastName3, "lives at", address3, "with an pay rate of", payRate3, "and was hired on", hireDate3);
 
 // output            
 /*
-Angel Padilla 1111 N. Blue Street 10.1 1/11/2011
-Frank Johnson 2222 W. Yellow Street 20.2 2/12/2012
-Carl Shaw 3333 S. Pink Street 30.3 3/13/2013
+Employee 1: Angel Padilla lives at 1111 N. Blue Street with an pay rate of 10.1 and was hired on 1/11/2011
+Employee 2: Frank Johnson lives at 2222 W. Yellow Street with an pay rate of 20.2 and was hired on 2/12/2012
+Employee 3: Carl Shaw lives at 3333 S. Pink Street with an pay rate of 30.3 and was hired on 3/13/2013
 */
 
 
