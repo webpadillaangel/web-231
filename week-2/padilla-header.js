cbf5982783dff5c92d12223d4aecc95456c2eba9
Angel Padilla <aapadilla@my365.bellevue.edu>
2019-12-04 23:40:20 -0600

    Updates

--- week-2/padilla-header.js
+++ week-2/padilla-header.js
@@ -9,13 +9,13 @@
 */
 
 /**
- * Params: firstName, lastName, assignment
- * Response: output
- * Description: Returns a well-formatted string header
- */
+* Params: firstName, lastName, assignment
+* Response: output
+* Description: Returns a well-formatted string header
+*/
 exports.display = function (firstName, lastName, assignment) {
-  let output = '\n' + firstName + ' ' + lastName + '\n' + assignment + '\nDate: ' +
-    new Date().toLocaleDateString()
+	let output = '\n' + firstName + ' ' + lastName + '\n' + assignment + '\nDate: ' +
+	new Date().toLocaleDateString('en-US')
 
-  return output
+	return output
 }
