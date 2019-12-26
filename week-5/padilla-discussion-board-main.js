7a4cfe7183e5ffa22e116470547d9a7e9fc62ae0
Angel Padilla <aapadilla@my365.bellevue.edu>
2019-12-25 23:51:10 -0600

    initial commit of main DB

---
+++ week-5/padilla-discussion-board-main.js
@@ -0,0 +1,30 @@
+// Importing padilla-header file
+var header = require("../padilla-header");
+
+/*
+============================================
+; Title:  padilla-discussion-board-main.js
+; Author: Angel Padilla
+; Date:   25 Dec 2019
+; Modified By:
+; Description: Program that contains two errors
+;===========================================
+*/
+
+// logging header
+console.log(header.display("Angel", "Padilla", "Exercise 5.1"));
+
+// declaring cars array of objects;
+var cars = [
+  { make: "Audi", model: "S3" },
+  { make: "BMW", model: "M3" },
+  [ make: "Ford", model: "Escort" }
+];
+
+// returning an a string array of the map() the is concatenating text with the make and model of the cars array
+var carModel = cars.Map(x => {
+  return `The make is: ${x.make} and the model is: ${x.model}`;
+});
+
+// Logging the result of the map() of the cars array
+console.log(carModel);
