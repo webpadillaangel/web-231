6afe37632c3fe89406500fd7795d78f385bf2488
Angel Padilla <aapadilla@my365.bellevue.edu>
2020-01-01 16:02:09 -0600

    exercise 5.3

--- week-5/padilla-object-collections.js
+++ week-5/padilla-object-collections.js
@@ -14,6 +14,7 @@ var header = require("../padilla-header");
 // logging header
 console.log(header.display("Angel", "Padilla", "Exercise 5.3"));
 
+// Composers array of objects
 var composers = [
   { firstName: "Ludwig", lastName: "Beethoven", genre: "Classical", rating: "8" },
   { firstName: "Wolfgang", lastName: "Mozart", genre: "Classical", rating: "10" },
@@ -24,6 +25,22 @@ var composers = [
 
 console.log("\n-- COMPOSERS --");
 
+// Iterating over composers by using the forEach(). Logging the text using dot notation.
 composers.forEach((x) => {
   console.log(`Last Name: ${x.lastName}, Genre: ${x.genre}, Rating: ${x.rating}`);
 });
+
+/*
+  Output:
+  Angel Padilla
+  Exercise 5.3
+  Date: 1/1/2020
+
+  -- COMPOSERS --
+  Last Name: Beethoven, Genre: Classical, Rating: 8
+  Last Name: Mozart, Genre: Classical, Rating: 10
+  Last Name: Bach, Genre: Classical, Rating: 9
+  Last Name: Haydn, Genre: Classical, Rating: 6
+  Last Name: Schubert, Genre: Classical, Rating: 5
+
+*/
